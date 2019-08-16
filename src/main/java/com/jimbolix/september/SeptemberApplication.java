120491fb69ffb987c115615c2c93c1c6da4f421f
jimbolix <lrhtomcat@163.com>
2019-08-16 10:19:12 +0800

    项目初始化

---
+++ src/main/java/com/jimbolix/september/SeptemberApplication.java
@@ -0,0 +1,13 @@
+package com.jimbolix.september;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeptemberApplication.class, args);
+    }
+
+}
