6797a132906936fd3b1cdf1c1836c290748ce788
jimbolix <lrhtomcat@163.com>
2019-08-19 17:19:38 +0800

    日志测试

--- src/main/java/com/jimbolix/september/SeptemberApplication.java
+++ src/main/java/com/jimbolix/september/SeptemberApplication.java
@@ -1,5 +1,7 @@
 package com.jimbolix.september;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeptemberApplication {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(SeptemberApplication.class);
+        logger.info("启动类启动");
         SpringApplication.run(SeptemberApplication.class, args);
     }
 
