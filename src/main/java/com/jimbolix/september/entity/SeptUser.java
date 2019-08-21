a57246072bc3e5558dd42a601bd3f77176901b25
jimbolix <lrhtomcat@163.com>
2019-08-21 16:54:05 +0800

    添加mybatis-plus配置

---
+++ src/main/java/com/jimbolix/september/entity/SeptUser.java
@@ -0,0 +1,60 @@
+package com.jimbolix.september.entity;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.jimbolix.september.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author jimbolix
+ * @since 2019-08-21
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+public class SeptUser extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    @TableField("userName")
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 用户状态,1-启用,-1禁用
+     */
+    @TableField("userState")
+    private Integer userState = -1;
+
+    /**
+     * 创建时间
+     */
+    @TableField("createTime")
+    private LocalDateTime createTime;
+
+    /**
+     * 描述
+     */
+    @TableField("userDesc")
+    private String userDesc;
+
+
+}
---
+++ src/main/java/com/jimbolix/september/mapper/SeptUserMapper.java
@@ -0,0 +1,16 @@
+package com.jimbolix.september.mapper;
+
+import com.jimbolix.september.entity.SeptUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author jimbolix
+ * @since 2019-08-21
+ */
+public interface SeptUserMapper extends BaseMapper<SeptUser> {
+
+}
